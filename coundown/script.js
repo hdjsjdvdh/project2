8b80ee35fb5e628057ae27c47106927b255ae485
hdjsjdvdh <94834735+hdjsjdvdh@users.noreply.github.com>
2023-03-19 12:59:11 +0530

    Add files via upload

---
+++ coundown/script.js
@@ -0,0 +1,27 @@
+const enddate = "3 oct 2023 12:00 am"
+
+document.getElementById("end-date").innerText = enddate;
+
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+   const end = new Date(enddate)
+   const now = new Date()
+   const diff = (end-now) / 1000;
+
+   if(diff,0) return
+
+   inputs[0].value = Math.floor(diff/3600/24);
+   inputs[1].value = Math.floor(diff/3600)%24;
+   inputs[2].value = Math.floor(diff/60)%60;
+   inputs[3].value = Math.floor(diff)%60;
+}
+
+clock();
+
+setInterval(
+    () => {
+      clock()
+    },
+    1000
+)
